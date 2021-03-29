9a886298bb7ecdd94137d271311a26868801f6c6
Arthur Novaes <arthurnovaes0@gmail.com>
2021-03-29 02:24:29 -0300

    finished the covariance matrix

--- tcc/tcc.ino
+++ tcc/tcc.ino
@@ -7,11 +7,11 @@
 float avg_acc_acc_lati;
 float avg_acc_acc_long;
 
-double speed_acc_lati = 0.0;
-double speed_acc_long = 0.0;
+float speed_acc_lati = 0.0;
+float speed_acc_long = 0.0;
 
-double posi_acc_lati = 0.0;
-double posi_acc_long = 0.0;
+float posi_acc_lati = 0.0;
+float posi_acc_long = 0.0;
 
 
 float DP_speed_acc_lati;
@@ -37,11 +37,9 @@ void setup() {
   Serial.begin(9600);
   while (!Serial) {}
 
-  Serial.print("OPA ALOU 1");
   // start communication with IMU
   statusA = IMU.begin();
 
-  Serial.print("OPA ALOU 2");
   
   if (statusA < 0) {
     Serial.println("IMU initialization unsuccessful");
@@ -51,103 +49,108 @@ void setup() {
     while (1) {}
   }
 
-  Serial.print("OPA ALOU");
   // setting the accelerometer full scale range to +/-8G 
   IMU.setAccelRange(MPU9250::ACCEL_RANGE_2G);
   // setting SRD to 19 for a 50 Hz update rate
   IMU.setSrd(19);
 
-  int acquires = 100; // num of data acquires to standard deviation
-//  float acc_acc_DP_lati[100];
-//  float acc_acc_DP_long[100];
-//  float speed_acc_DP_lati[100];
-//  float speed_acc_DP_long[100];
-//  float posi_acc_DP_lati[100];
-//  float posi_acc_DP_long[100];
-//
+  int acquires = 10; // num of data acquires to standard deviation
+  float acc_acc_DP_lati[10];
+  float acc_acc_DP_long[10];
+  float speed_acc_DP_lati[10];
+  float speed_acc_DP_long[10];
+  float posi_acc_DP_lati[10];
+  float posi_acc_DP_long[10];
+
   float sum_acc_acc_lati = 0.0;
   float sum_acc_acc_long = 0.0;
-//  float sum_speed_acc_lati = 0.0;
-//  float sum_speed_acc_long = 0.0;
-//  float sum_posi_acc_lati = 0.0;
-//  float sum_posi_acc_long = 0.0;
-//  
+  float sum_speed_acc_lati = 0.0;
+  float sum_speed_acc_long = 0.0;
+  float sum_posi_acc_lati = 0.0;
+  float sum_posi_acc_long = 0.0;
+  
   for (int i = 0; i < acquires; i++) {
-//    IMU.readSensor();
-    float acc_acc_lati = IMU.getAccelX_mss();
-//    acc_acc_DP_lati[i] = acc_acc_lati;
-    float acc_acc_long = IMU.getAccelY_mss();
-//    acc_acc_DP_lati[i] = acc_acc_lati;
-//    
-    sum_acc_acc_lati += acc_acc_lati;
-    sum_acc_acc_long += acc_acc_long;
-//   
-//    
-//    speed_acc_lati += acc_acc_lati; // TODO:  precisaria multiplicar algum tempo aqui? mesmo não tendo o delalong
-//    speed_acc_DP_lati[i] = speed_acc_lati;
-//    speed_acc_long += acc_acc_long;
-//    speed_acc_DP_long[i] = speed_acc_long;
-//
-//    sum_speed_acc_lati += speed_acc_lati;
-//    sum_speed_acc_long += speed_acc_long;
-//
-//
-//    posi_acc_lati += speed_acc_lati; // TODO:  precisaria multiplicar algum tempo aqui? mesmo não tendo o delalong
-//    posi_acc_DP_lati[i] = posi_acc_lati;
-//    posi_acc_long += speed_acc_long;
-//    posi_acc_DP_long[i] = posi_acc_long;
-//
-//    sum_posi_acc_lati += posi_acc_lati;
-//    sum_posi_acc_long += posi_acc_long;
+    IMU.readSensor();
+    acc_acc_DP_lati[i] = IMU.getAccelX_mss();
+    acc_acc_DP_long[i] = IMU.getAccelY_mss();
+  
+    sum_acc_acc_lati += acc_acc_DP_lati[i];
+    sum_acc_acc_long += acc_acc_DP_long[i];
+
+
+    speed_acc_lati += acc_acc_DP_lati[i] * 0.02;
+    speed_acc_DP_lati[i] = speed_acc_lati;
+    speed_acc_long += acc_acc_DP_long[i] * 0.02;
+    speed_acc_DP_long[i] = speed_acc_long;
+    
+    sum_speed_acc_lati += speed_acc_DP_lati[i];
+    sum_speed_acc_long += speed_acc_DP_long[i];
+
+
+    posi_acc_lati += speed_acc_DP_lati[i] * 0.02;
+    posi_acc_DP_lati[i] = posi_acc_lati;
+    posi_acc_long += speed_acc_DP_long[i] * 0.02;
+    posi_acc_DP_long[i] = posi_acc_long;
+
+    sum_posi_acc_lati += posi_acc_DP_lati[i];
+    sum_posi_acc_long += posi_acc_DP_long[i];
+
+    delay(20);
   }
-//  
+
+  delay(2000);
+  
   avg_acc_acc_lati = sum_acc_acc_lati / acquires;
   avg_acc_acc_long = sum_acc_acc_long / acquires;
-//  float avg_speed_acc_lati = sum_speed_acc_lati / acquires;
-//  float avg_speed_acc_long = sum_speed_acc_long / acquires;
-//  float avg_posi_acc_lati = sum_posi_acc_lati / acquires;
-//  float avg_posi_acc_long = sum_posi_acc_long / acquires;
-//  
-//
-//  float aulati_DP_acc_acc_lati = 0;
-//  float aulati_DP_acc_acc_long = 0;
-//  float aulati_DP_speed_acc_lati = 0;
-//  float aulati_DP_speed_acc_long = 0;
-//  float aulati_DP_posi_acc_lati = 0;
-//  float aulati_DP_posi_acc_long = 0;
-//  for (int i = 0; i < acquires; i++) {
-//    aulati_DP_acc_acc_lati += (acc_acc_DP_lati[i] - avg_acc_acc_lati) * (acc_acc_DP_lati[i] - avg_acc_acc_lati);
-//    aulati_DP_acc_acc_long += (acc_acc_DP_long[i] - avg_acc_acc_long) * (acc_acc_DP_long[i] - avg_acc_acc_long);
-//    
-//    aulati_DP_speed_acc_lati += (speed_acc_DP_lati[i] - avg_speed_acc_lati) * (speed_acc_DP_lati[i] - avg_speed_acc_lati);
-//    aulati_DP_speed_acc_long += (speed_acc_DP_long[i] - avg_speed_acc_long) * (speed_acc_DP_long[i] - avg_speed_acc_long);
-//
-//    aulati_DP_posi_acc_lati += (posi_acc_DP_lati[i] - avg_posi_acc_lati) * (posi_acc_DP_lati[i] - avg_posi_acc_lati);
-//    aulati_DP_posi_acc_long += (posi_acc_DP_long[i] - avg_posi_acc_long) * (posi_acc_DP_long[i] - avg_posi_acc_long);
-//  }
-//
-//  float DP_acc_acc_lati = sqrt(aulati_DP_acc_acc_lati / acquires);
-//  float DP_acc_acc_long = sqrt(aulati_DP_acc_acc_lati / acquires);
-//
-//  DP_speed_acc_lati = sqrt(aulati_DP_speed_acc_lati / acquires);
-//  DP_speed_acc_long = sqrt(aulati_DP_speed_acc_long / acquires);
-//
-//  DP_posi_acc_lati = sqrt(aulati_DP_posi_acc_lati / acquires);
-//  DP_posi_acc_long = sqrt(aulati_DP_posi_acc_long / acquires);
-//  
-//
-//
-//  // zera novamente a velocidade e a posição
-//  speed_acc_lati = 0.0;
-//  speed_acc_long = 0.0;
-//
-//  posi_acc_lati = 0.0;
-//  posi_acc_long = 0.0;
-//
-//  // Acabou a aquisição de dados para a matriz de covariancia
+  float avg_speed_acc_lati = sum_speed_acc_lati / acquires;
+  float avg_speed_acc_long = sum_speed_acc_long / acquires;
+  float avg_posi_acc_lati = sum_posi_acc_lati / acquires;
+  float avg_posi_acc_long = sum_posi_acc_long / acquires;
+  
+
+  float auxlati_DP_acc_acc_lati = 0.0;
+  float auxlong_DP_acc_acc_long = 0.0;
+  float auxlati_DP_speed_acc_lati = 0.0;
+  float auxlong_DP_speed_acc_long = 0.0;
+  float auxlati_DP_posi_acc_lati = 0.0;
+  float auxlong_DP_posi_acc_long = 0.0;
+
+  Serial.println("==================");
+
+  for (int i = 0; i < acquires; i++) {
+    auxlati_DP_acc_acc_lati += (acc_acc_DP_lati[i] - avg_acc_acc_lati) * (acc_acc_DP_lati[i] - avg_acc_acc_lati);
+    auxlong_DP_acc_acc_long += (acc_acc_DP_long[i] - avg_acc_acc_long) * (acc_acc_DP_long[i] - avg_acc_acc_long);
+    
+    auxlati_DP_speed_acc_lati += (speed_acc_DP_lati[i] - avg_speed_acc_lati) * (speed_acc_DP_lati[i] - avg_speed_acc_lati);
+    auxlong_DP_speed_acc_long += (speed_acc_DP_long[i] - avg_speed_acc_long) * (speed_acc_DP_long[i] - avg_speed_acc_long);
+    
+    auxlati_DP_posi_acc_lati += (posi_acc_DP_lati[i] - avg_posi_acc_lati) * (posi_acc_DP_lati[i] - avg_posi_acc_lati);
+    auxlong_DP_posi_acc_long += (posi_acc_DP_long[i] - avg_posi_acc_long) * (posi_acc_DP_long[i] - avg_posi_acc_long);
+  }
+
+
+  float DP_acc_acc_lati = sqrt(auxlati_DP_acc_acc_lati / acquires);
+  float DP_acc_acc_long = sqrt(auxlong_DP_acc_acc_long / acquires);
+
+  DP_speed_acc_lati = sqrt(auxlati_DP_speed_acc_lati / acquires);
+  DP_speed_acc_long = sqrt(auxlong_DP_speed_acc_long / acquires);
+
+  DP_posi_acc_lati = sqrt(auxlati_DP_posi_acc_lati / acquires);
+  DP_posi_acc_long = sqrt(auxlong_DP_posi_acc_long / acquires);
+
+  // zera novamente a velocidade e a posição
+  speed_acc_lati = 0.0;
+  speed_acc_long = 0.0;
+
+  posi_acc_lati = 0.0;
+  posi_acc_long = 0.0;
+ 
+  // Acabou a aquisição de dados para a matriz de covariancia
   digitalWrite(CALIB_PENDING, LOW);
   digitalWrite(CALIB_DONE, HIGH);
+
 }
+
 
 float acc_acc_lati;
 float acc_acc_long;
@@ -220,15 +223,17 @@ void loop() {
   xk_long[1][0] = ((A[1][0] * x_long[0][0]) + (A[1][1] * x_long[1][0])) + (B[1][0] * u_long[0]);
 
   double speedTotal_state = sqrt((xk_lati[1][0]*xk_lati[1][0]) + (xk_long[1][0]*xk_long[1][0]));
-  double speedTotal_int = sqrt((speed_acc_lati*speed_acc_lati) + (speed_acc_long*speed_acc_long));
+  double posiTotal_state = sqrt((xk_lati[0][0]*xk_lati[0][0]) + (xk_long[0][0]*xk_long[0][0]));
+  
 
-  Serial.print(speedTotal_int + 0.1);
+  Serial.print(posiTotal_state + 0.1);
   Serial.print(" ");
   Serial.print(speedTotal_state);
   Serial.print(" ");
   Serial.print(acc_acc_long);
   Serial.print(" ");
   Serial.println(acc_acc_lati);
+  
   
   delay(20);
 }
